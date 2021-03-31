5dbbee5a23c6dc2e2845ba90f9bc98a848efcb12
Rokas <rokas@rokas.com>
2021-03-31 12:47:56 +0300

    Iniitial commit

---
+++ courses/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Course
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('id', 'url', 'name', 'language', 'price')
